bd901645712e19f1928bbc37713b4862c00632aa
Danilo Bandeira <danilobandeira29@gmail.com>
2024-10-26 12:04:55 -0300

    fix: validate value to be at least 0.1

--- src/main/java/com/github/danilobandeira29/easy_funds/controllers/TransferRequestDto.java
+++ src/main/java/com/github/danilobandeira29/easy_funds/controllers/TransferRequestDto.java
@@ -1,14 +1,14 @@
 package com.github.danilobandeira29.easy_funds.controllers;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public record TransferRequestDto(
         @NotNull(message = "value must be float")
-        @Positive(message = "value must be greater than zero")
+        @DecimalMin(value = "0.1", message = "value must be at least 0.1")
         BigDecimal value,
         @NotNull(message = "payer must be a valid uuid")
         UUID payer,
